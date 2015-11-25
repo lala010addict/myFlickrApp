9794722c746e54954789cba16407674071a680f4
JieYing Peng <jipeng@syr.edu>
2015-11-25 02:05:54 -0800

    a

--- flickr angular/server/config/db.js
+++ flickr angular/server/config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'url': 'mongodb://localhost/flickrusers'
+  'url': 'mongodb://localhost/flickrusers' || "mongodb://<dbuser>:<dbpassword>@ds059284.mongolab.com:59284/heroku_5qtg5m2v"
 }
