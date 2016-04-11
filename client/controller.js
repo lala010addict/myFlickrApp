27d9dfb7843a4acaa0e21004455e69ba34e991a5
jieyingpeng <jipeng@syr.edu>
2016-04-11 12:27:08 -0700

    favs only displaying on appropriate user page

--- client/controller.js
+++ client/controller.js
@@ -22,18 +22,18 @@ myFlickr.controller('menu', ['$scope', 'Auth', "$http", 'PhotoSet', "$location",
             response.$promise.then(function(data) {
 
                 $scope.userInfo = data.toJSON(); //Changed data.data.topics to data.topics
-                // console.log($scope.userInfo)
+                // //console.log($scope.userInfo)
                 if ($scope.userInfo.name === $scope.username) {
                     $scope.isLoggedIn = true;
                     $scope.checked = true;
-                    console.log($scope.userInfo)
+                    //console.log($scope.userInfo)
                     $scope.formData.user_id = $scope.userInfo._id;
                     $scope.formData.username = $scope.userInfo.name
 
                 } else {
                     $scope.isLoggedIn = false;
                     $scope.checked = false;
-                    console.log('nonono')
+                    //console.log('nonono')
                 }
 
             });
@@ -50,19 +50,25 @@ myFlickr.controller('menu', ['$scope', 'Auth', "$http", 'PhotoSet', "$location",
     var checkifLiked = function() {
         $http.get('/api/favorites')
             .success(function(data) {
+                console.log(data)
 
                 if (data[0].username === $scope.username) {
-                    $scope.favoritePictures = data;
-                    console.log('checkiffollowed', $scope.favoritePictures)
 
-                    var ids = _.pluck(data, 'picture_id')
+                    //console.log('checkiffollowed', $scope.favoritePictures)
+
+                    var filteredData = _.filter(data, function(name) {
+                        return name.username === $scope.username
+                    })
+                    console.log(filteredData, 'filteredData')
+                    $scope.favoritePictures = filteredData;
+                    var ids = _.pluck(filteredData, 'picture_id')
 
 
                     var newPhotoArray = $scope.photos.filter(function(obj) {
                         return ids.indexOf(obj.id) === -1;
                     });
 
-                    console.log(newPhotoArray)
+                    //console.log(newPhotoArray)
 
                     $scope.photos = newPhotoArray;
                 }
@@ -71,7 +77,7 @@ myFlickr.controller('menu', ['$scope', 'Auth', "$http", 'PhotoSet', "$location",
 
             })
             .error(function(data) {
-                console.log('Error: ' + data);
+                //console.log('Error: ' + data);
             });
     }
 
@@ -83,7 +89,7 @@ myFlickr.controller('menu', ['$scope', 'Auth', "$http", 'PhotoSet', "$location",
         $scope.formData.picture_secret = secret;
         $http.post('/api/favorites', $scope.formData)
             .then(function(data) {
-                console.log(data);
+                //console.log(data);
                 $scope.getUser($scope.username);
             })
 
@@ -99,7 +105,7 @@ myFlickr.controller('menu', ['$scope', 'Auth', "$http", 'PhotoSet', "$location",
                 $scope.getUser($scope.username);
             })
             .error(function(data) {
-                console.log('Error: ' + data);
+                //console.log('Error: ' + data);
             });
 
     }
@@ -108,16 +114,14 @@ myFlickr.controller('menu', ['$scope', 'Auth', "$http", 'PhotoSet', "$location",
     $scope.all = function(id) {
 
         $scope.photos = id.slice(0, 300)
-        console.log($scope.photos)
+            //console.log($scope.photos)
     }
     $scope.username = '';
 
 
     // $routeParams.username =  $scope.username
     //|| "booh!";
-    $scope.click = function(e) {
-        alert(e)
-    }
+
 
 
     $http.get("/:name").success(function(data) {
@@ -138,14 +142,14 @@ myFlickr.controller('menu', ['$scope', 'Auth', "$http", 'PhotoSet', "$location",
     $scope.getSet = function(userID) {
         PhotoSet.getSet(userID)
             .success(function(data) {
-                //  console.log(data.photosets.photoset)
+                //  //console.log(data.photosets.photoset)
                 if (data.photosets.photoset.length < 1) {
                     alert("NO PHOTO ALBUMS FOUND FOR THIS USER");
                 } else {
                     $scope.data = data.photosets.photoset
-                        // console.log(data.photosets.photoset)
+                        // //console.log(data.photosets.photoset)
                     _.each(data.photosets.photoset, function(item) {
-                        // console.log(item.id)
+                        // //console.log(item.id)
                         $scope.getPhotos(item.id)
                     })
                 }
@@ -158,19 +162,19 @@ myFlickr.controller('menu', ['$scope', 'Auth', "$http", 'PhotoSet', "$location",
     //$scope.getSet()
 
     $scope.getPhotos = function(item) {
-        //  console.log(item);
+        //  //console.log(item);
         PhotoSet.getPhotos(item)
             .success(function(data) {
 
                 $scope.photos = data.photoset.photo.slice(0, 100)
                 checkifLiked();
-                // console.log("hihi", data.photoset.photo)
+                // //console.log("hihi", data.photoset.photo)
                 _.each(data.photoset.photo, function(item) {
 
                     if ($scope.idHolder.indexOf(item.id) === -1) {
-                        // console.log(item.id)
+                        // //console.log(item.id)
                         $scope.idHolder.push(item.id)
-                            //   console.log("more thing got pushed")
+                            //   //console.log("more thing got pushed")
                         $scope.pix.push(item)
                     }
                 })
@@ -183,11 +187,11 @@ myFlickr.controller('menu', ['$scope', 'Auth', "$http", 'PhotoSet', "$location",
         checkIfLoggedIn();
         $scope.pix = [];
         $scope.idHolder = []
-        console.log(username)
+            //console.log(username)
         PhotoSet.getUser(username)
 
         .success(function(data) {
-            console.log(data)
+            //console.log(data)
             if (data.code === 1 || undefined) {
                 alert(data.message)
             } else {
